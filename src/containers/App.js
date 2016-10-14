ba13352f2fdb3fc92cf6eebebb5ffe6ec7b9784e
Serhii Strizhak <Serhii_Strizhak@epam.com>
2016-10-14 15:50:14 +0300

    setup env

---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return <div>This is the React bitch!</div>
+  }
+}
+
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './containers/App'
+
+
+
+render (
+  <App />,
+  document.getElementById('root')
+)
+
+
+module.hot.accept();
